4e7a6f20f28bb2e486475326396ea2d71b9f5bb2
Denis Florkin <denisflorkin@gmail.com>
2019-01-29 18:35:47 +0100

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,94 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import {
+  c as cScale,
+  d as dScale,
+} from './data/scales'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+const defaultRange = 4;
+
+const notesPosition = {
+  c: 40,
+  d: 36,
+  e: 32,
+  f: 28,
+  g: 24,
+  a: 20,
+  b: 16
+};
+
+const WIDTH = 800;
+const HEIGHT = 48;
+
+const tempoBase = 4;
+const tempoRythm = 4;
+
+export default function App() {
+  return (
+    <div style={{ border: "1px solid grey", padding: "32px 0" }}>
+      <div style={{ maxWidth: "800px", margin: "0 auto" }} >
+        <svg viewBox={`0 0 ${WIDTH} ${HEIGHT}`}>
+          {/* base Staff */}
+          {[...new Array(5)].map((_, i) => (
+            <line
+              x1="0"
+              y1={i * 8}
+              x2={WIDTH}
+              y2={i * 8}
+              style={{ stroke: "black", strokeWidth: 1 }}
+            />
+          ))}
+
+          {/* bars */}
+          {[...new Array(5)].map((_, i) => (
+            <line
+              x1={i * (WIDTH / tempoBase)}
+              y1={0}
+              x2={i * (WIDTH / tempoBase)}
+              y2={32}
+              style={{ stroke: "black", strokeWidth: 1 }}
+            />
+          ))}
+
+          {/* Notes */}
+          {cScale.map(({ name, beatPosition, sharp, range }) => (
+            <g>
+              {
+                sharp
+                  ? <text>#</text>
+                  : null
+              }
+              <circle
+                cx={beatPosition * (WIDTH / tempoBase / tempoRythm)}
+                cy={notesPosition[name] - ((range - defaultRange) * 28 )}
+                r="4"
+              />
+            </g>
+          ))}
+
+          {/* Notes */}
+          {dScale.map(({ name, beatPosition, sharp, range }) => (
+            <g>
+              {
+                sharp
+                  ? <text
+                      style={{ fontWeight: 'thin' }}
+                      x={(beatPosition * (WIDTH / tempoBase / tempoRythm)) - 16}
+                      y={(notesPosition[name] + 6)  - ((range - defaultRange) * 28 )}
+                    >
+                      #
+                    </text>
+                  : null
+              }
+              <circle
+                cx={beatPosition * (WIDTH / tempoBase / tempoRythm)}
+                cy={notesPosition[name] - ((range - defaultRange) * 28 )}
+                r="4"
+              />
+            </g>
+          ))}
+        </svg>
       </div>
-    );
-  }
+    </div>
+  );
 }
-
-export default App;
---
+++ src/data/scales.js
@@ -0,0 +1,111 @@
+import { lt } from '../utils'
+
+export const c = [
+  {
+    range: 4,
+    name: "c",
+    beatPosition: lt(0, 1, 7, 1, 4),
+    type: "half"
+  },
+  {
+    range: 4,
+    name: "d",
+    beatPosition: lt(1, 1, 7, 1, 4),
+    type: "half"
+  },
+  {
+    range: 4,
+    name: "e",
+    beatPosition: lt(2, 1, 7, 1, 4),
+    type: "half"
+  },
+  {
+    range: 4,
+    name: "f",
+    beatPosition: lt(3, 1, 7, 1, 4),
+    type: "half"
+  },
+  {
+    range: 4,
+    name: "g",
+    beatPosition: lt(4, 1, 7, 1, 4),
+    type: "half"
+  },
+  {
+    range: 4,
+    name: "a",
+    beatPosition: lt(5, 1, 7, 1, 4),
+    type: "half"
+  },
+  {
+    range: 4,
+    name: "b",
+    beatPosition: lt(6, 1, 7, 1, 4),
+    type: "half"
+  },
+  {
+    range: 5,
+    name: "c",
+    beatPosition: lt(7, 1, 7, 1, 4),
+    type: "half"
+  }
+];
+
+export const d = [
+  {
+    range: 4,
+    name: "d",
+    beatPosition: lt(8, 1, 7, 1, 4),
+    type: "half"
+  },
+  {
+    range: 4,
+    name: "e",
+    beatPosition: lt(9, 1, 7, 1, 4),
+    type: "half"
+  },
+  {
+    range: 4,
+    name: "f",
+    sharp: true,
+    beatPosition: lt(10, 1, 7, 1, 4),
+    type: "half"
+  },
+  {
+    range: 4,
+    name: "g",
+    beatPosition: lt(11, 1, 7, 1, 4),
+    type: "half"
+  },
+  {
+    range: 4,
+    name: "a",
+    beatPosition: lt(12, 1, 7, 1, 4),
+    type: "half"
+  },
+  {
+    range: 4,
+    name: "b",
+    beatPosition: lt(13, 1, 7, 1, 4),
+    type: "half"
+  },
+  {
+    range: 5,
+    name: "c",
+    sharp: true,
+    beatPosition: lt(14, 1, 7, 1, 4),
+    type: "half"
+  },
+  {
+    range: 5,
+    name: "d",
+    beatPosition: lt(15, 1, 7, 1, 4),
+    type: "half"
+  }
+];
+
+
+export default {
+  c,
+  d,
+}
